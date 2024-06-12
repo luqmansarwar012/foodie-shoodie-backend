89cd62dc42d2678a8e5b39b5dbc97d28057c4be0
Raja Luqman Sarwar <luqmanrajput012@gmail.com>
2024-06-12 04:10:58 -0700

    multer config update

--- config/multer.js
+++ config/multer.js
@@ -2,7 +2,9 @@ import multer from "multer";
 
 // Image Storage engine
 const storage = multer.diskStorage({
-  destination: "uploads",
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/uploads");
+  },
   filename: (req, file, cb) => {
     return cb(null, `${Date.now()}${file.originalname}`);
   },
